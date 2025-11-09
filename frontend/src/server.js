895f1013e7292b9b4616ec97c4c1e68291b13143
Ali Asad <aliasad0443@gmail.com>
2025-11-10 00:32:45 +0500

    url update

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://e-shop-project-669m.vercel.app/api/v2";
+export const server = "https://e-shop-project-pink.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://e-shop-project-pink.vercel.app/";

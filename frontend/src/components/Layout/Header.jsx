87e00c765c213cfe026fdaad4e263afaaf0cdbeb
Ali Asad <aliasad0443@gmail.com>
2025-11-15 00:26:35 +0500

    wishlist bug

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -262,7 +262,7 @@ const Header = ({ activeHeading }) => {
                   >
                     <AiOutlineHeart size={30} className="mt-5 ml-3" />
                     <span class="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px]  leading-tight text-center">
-                      {/* {wishlist && wishlist.length} */}0
+                      {wishlist && wishlist.length}
                     </span>
                   </div>
                 </div>
